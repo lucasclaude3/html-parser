ff4c18364d7e49d0d5804af0799f0eaae46fa79b
kahlua <lucasclaude3@gmail.com>
2019-04-14 16:40:25 +0200

    Refactor for the sake of clarity

--- html-parser.js
+++ html-parser.js
@@ -41,55 +41,9 @@ function parseHtml(formattedHtml) {
   const code = document.querySelector('.link.aftersale-web').innerHTML.trim().match(/pnrRef=([^\& ]*)/)[1];
   const name = getContentByQuerySelector(document, '.pnr-name .pnr-info');
   const price = parseFloat(getContentByQuerySelector(document, '.total-amount .very-important').replace(/[^\d,.]/g, '').replace(/,/g, '.'));
-
-  const products = document.getElementsByClassName('product-details');
-
-  const callback = (document, product) => {
-    const day = getContentByQuerySelector(product.previousElementSibling, '.product-travel-date');
-    const dateWithoutYear = moment.utc(day, "DD MMMM", "fr", false);
-
-    const dateWithoutYearString = dateWithoutYear.format('[\]DD[\/]MM[\/]([\\d*])');
-    const dateWithoutYearRegex = new RegExp(dateWithoutYearString);
-    const year = document.body.innerHTML.match(dateWithoutYearRegex);
-    const date = dateWithoutYear.set('year', year[1]).toISOString().replace(/T/, ' ');
-
-    const travelWay = getContentByQuerySelector(product, '.travel-way');
-    const departureTime = getContentByQuerySelector(product, '.origin-destination-hour.segment-departure').replace(/h/, ':');
-
-    const departureStationElement = product.querySelector('.origin-destination-station.segment-departure');
-
-    const departureStation = departureStationElement.innerHTML.trim();
-    const travelType = departureStationElement.nextElementSibling.innerHTML.trim();
-    const number = departureStationElement.nextElementSibling.nextElementSibling.innerHTML.trim();
-
-    const arrivalTime = getContentByQuerySelector(product, '.origin-destination-hour.segment-arrival').replace(/h/, ':');
-    const arrivalStation = getContentByQuerySelector(product, '.origin-destination-station.segment-arrival');
-
-    const passengerTypologyElements = product.nextElementSibling.getElementsByClassName('typology');
-    const passengers = Array.prototype.map.call(passengerTypologyElements, passengerTypologyElement => {
-      const typology = passengerTypologyElement.innerHTML.trim().match(/(\(.*\))/)[0];
-      const fareType = passengerTypologyElement.nextElementSibling.innerHTML.trim().match(/Billet (échangeable)/)[1];
-      return { age: typology, type: fareType };
-    });
-
-    return {
-      type: travelWay,
-      date: date,
-      trains: [
-        {
-          departureTime,
-          departureStation,
-          arrivalTime,
-          arrivalStation,
-          type: travelType,
-          number,
-          passengers
-        }
-      ]
-    }
-  }
-
-  const roundTrips = Array.prototype.map.call(products, callback.bind(null, document));
+  
+  const products = document.querySelectorAll('.product-details');
+  const roundTrips = Array.prototype.map.call(products, productCallback.bind(null, document));
 
   const priceElements = document.querySelectorAll('.product-header>tbody>tr>td:last-child, .product-header>.amount');
   const prices = Array.prototype.map.call(priceElements, function (priceElement) {
@@ -115,6 +69,58 @@ function parseHtml(formattedHtml) {
   }
 
   return result;
+}
+
+function productCallback(document, product) {
+  const travelWay = getContentByQuerySelector(product, '.travel-way');
+  const departureTime = getContentByQuerySelector(product, '.origin-destination-hour.segment-departure').replace(/h/, ':');
+  const departureStationElement = product.querySelector('.origin-destination-station.segment-departure');
+  const departureStation = departureStationElement.innerHTML.trim();
+  const travelType = departureStationElement.nextElementSibling.innerHTML.trim();
+  const number = departureStationElement.nextElementSibling.nextElementSibling.innerHTML.trim();
+  const arrivalTime = getContentByQuerySelector(product, '.origin-destination-hour.segment-arrival').replace(/h/, ':');
+  const arrivalStation = getContentByQuerySelector(product, '.origin-destination-station.segment-arrival');
+  const passengers = getProductPassengers(product);
+  const date = getProductDate(document, product);
+
+  return {
+    type: travelWay,
+    date,
+    trains: [
+      {
+        departureTime,
+        departureStation,
+        arrivalTime,
+        arrivalStation,
+        type: travelType,
+        number,
+        passengers
+      }
+    ]
+  }
+}
+
+function getProductPassengers(product) {
+  const passengerTypologyElements = product.nextElementSibling.querySelectorAll('.typology');
+  const passengers = Array.prototype.map.call(passengerTypologyElements, passengerTypologyElement => {
+    const typology = passengerTypologyElement.innerHTML.trim().match(/(\(.*\))/)[0];
+    const fareType = passengerTypologyElement.nextElementSibling.innerHTML.trim().match(/Billet (échangeable)/)[1];
+    
+    return { age: typology, type: fareType };
+  });
+
+  return passengers
+}
+
+function getProductDate(document, product) {
+  const day = getContentByQuerySelector(product.previousElementSibling, '.product-travel-date');
+  const dateWithoutYear = moment.utc(day, "DD MMMM", "fr", false);
+  const dateWithoutYearString = dateWithoutYear.format('[\]DD[\/]MM[\/]([\\d*])');
+  const dateWithoutYearRegex = new RegExp(dateWithoutYearString);
+  const year = document.body.innerHTML.match(dateWithoutYearRegex);
+  const date = dateWithoutYear.set('year', year[1]).toISOString().replace(/T/, ' ');
+
+  return date;
 }
 
 function getContentByQuerySelector(element, querySelector) {
