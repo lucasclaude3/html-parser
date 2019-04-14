616250072cb7570a6ac202cbb313bc66951e8f94
kahlua <lucasclaude3@gmail.com>
2019-04-14 16:55:17 +0200

    Add linter

--- html-parser.js
+++ html-parser.js
@@ -1,19 +1,14 @@
 const Promise = require('bluebird');
 const fs = Promise.promisifyAll(require('fs'));
-const jsdom = require('jsdom');
-const { JSDOM } = jsdom;
+const { JSDOM } = require('jsdom');
 const moment = require('moment');
 
-exports.loadAndParseHtml = function (filename) {
-  return fs.readFileAsync(filename, 'utf8')
-    .then(function (rawHtml) {
-      formattedHtml = formatHtml(rawHtml);
-      return writeFormattedHtml(formattedHtml, 'formatted-' + filename);
-    })
-    .then(function (formattedHtml) {
-      parsedHtml = parseHtml(formattedHtml);
-      return writeJsonFile(parsedHtml, 'parsed-' + filename.split('.').shift() + '.json');
-    });
+function formatHtml(rawHtml) {
+  let result = rawHtml.replace(/\\"/g, '"');
+  result = result.replace(/\\n/g, '\n');
+  result = result.replace(/\\r/g, '');
+
+  return result;
 }
 
 function writeFormattedHtml(formattedHtml, filename) {
@@ -26,49 +21,31 @@ function writeJsonFile(jsonObject, filename) {
     .then(() => jsonObject);
 }
 
-function formatHtml(rawHtml) {
-  let result = rawHtml.replace(/\\"/g, '"');
-  result = result.replace(/\\n/g, '\n');
-  result = result.replace(/\\r/g, '');
-  
-  return result;
+function getContentByQuerySelector(element, querySelector) {
+  return element.querySelector(querySelector).innerHTML.trim();
 }
 
-function parseHtml(formattedHtml) {
-  const dom = new JSDOM(formattedHtml);
-  const document = dom.window.document;
+function getProductPassengers(product) {
+  const passengerElements = product.nextElementSibling.querySelectorAll('.typology');
+  const passengers = Array.prototype.map.call(passengerElements, (passengerElement) => {
+    const typology = passengerElement.innerHTML.trim().match(/(\(.*\))/)[0];
+    const fareType = passengerElement.nextElementSibling.innerHTML.trim().match(/Billet (échangeable)/)[1];
 
-  const code = document.querySelector('.link.aftersale-web').innerHTML.trim().match(/pnrRef=([^\& ]*)/)[1];
-  const name = getContentByQuerySelector(document, '.pnr-name .pnr-info');
-  const price = parseFloat(getContentByQuerySelector(document, '.total-amount .very-important').replace(/[^\d,.]/g, '').replace(/,/g, '.'));
-  
-  const products = document.querySelectorAll('.product-details');
-  const roundTrips = Array.prototype.map.call(products, productCallback.bind(null, document));
-
-  const priceElements = document.querySelectorAll('.product-header>tbody>tr>td:last-child, .product-header>.amount');
-  const prices = Array.prototype.map.call(priceElements, function (priceElement) {
-    return { value: parseFloat(priceElement.innerHTML.trim().replace(/[^\d,]/, '').replace(/,/, '.')) };
+    return { age: typology, type: fareType };
   });
 
-  const result = {
-    status: "ok",
-    result: {
-      trips: [
-        { code,
-          name,
-          details: {
-            price,
-            roundTrips,
-          }
-        }
-      ],
-      custom: {
-        prices,
-      }
-    }
-  }
+  return passengers;
+}
 
-  return result;
+function getProductDate(document, product) {
+  const day = getContentByQuerySelector(product.previousElementSibling, '.product-travel-date');
+  const dateWithoutYear = moment.utc(day, 'DD MMMM', 'fr', false);
+  const dateWithoutYearString = dateWithoutYear.format('DD/MM/([\\d*])');
+  const dateWithoutYearRegex = new RegExp(dateWithoutYearString);
+  const year = document.body.innerHTML.match(dateWithoutYearRegex);
+  const date = dateWithoutYear.set('year', year[1]).toISOString().replace(/T/, ' ');
+
+  return date;
 }
 
 function productCallback(document, product) {
@@ -94,35 +71,54 @@ function productCallback(document, product) {
         arrivalStation,
         type: travelType,
         number,
-        passengers
-      }
-    ]
-  }
+        passengers,
+      },
+    ],
+  };
 }
 
-function getProductPassengers(product) {
-  const passengerTypologyElements = product.nextElementSibling.querySelectorAll('.typology');
-  const passengers = Array.prototype.map.call(passengerTypologyElements, passengerTypologyElement => {
-    const typology = passengerTypologyElement.innerHTML.trim().match(/(\(.*\))/)[0];
-    const fareType = passengerTypologyElement.nextElementSibling.innerHTML.trim().match(/Billet (échangeable)/)[1];
-    
-    return { age: typology, type: fareType };
+function parseHtml(formattedHtml) {
+  const dom = new JSDOM(formattedHtml);
+  const { document } = dom.window;
+
+  const code = document.querySelector('.link.aftersale-web').innerHTML.trim().match(/pnrRef=([^& ]*)/)[1];
+  const name = getContentByQuerySelector(document, '.pnr-name .pnr-info');
+  const price = parseFloat(getContentByQuerySelector(document, '.total-amount .very-important').replace(/[^\d,.]/g, '').replace(/,/g, '.'));
+
+  const products = document.querySelectorAll('.product-details');
+  const roundTrips = Array.prototype.map.call(products, productCallback.bind(null, document));
+
+  const priceElements = document.querySelectorAll('.product-header>tbody>tr>td:last-child, .product-header>.amount');
+  const prices = Array.prototype.map.call(priceElements, priceElement => ({ value: parseFloat(priceElement.innerHTML.trim().replace(/[^\d,]/, '').replace(/,/, '.')) }));
+
+  const result = {
+    status: 'ok',
+    result: {
+      trips: [
+        {
+          code,
+          name,
+          details: {
+            price,
+            roundTrips,
+          },
+        },
+      ],
+      custom: {
+        prices,
+      },
+    },
+  };
+
+  return result;
+}
+
+exports.loadAndParseHtml = filename => fs.readFileAsync(filename, 'utf8')
+  .then((rawHtml) => {
+    const formattedHtml = formatHtml(rawHtml);
+    return writeFormattedHtml(formattedHtml, `formatted-${filename}`);
+  })
+  .then((formattedHtml) => {
+    const parsedHtml = parseHtml(formattedHtml);
+    return writeJsonFile(parsedHtml, `parsed-${filename.split('.').shift()}.json`);
   });
-
-  return passengers
-}
-
-function getProductDate(document, product) {
-  const day = getContentByQuerySelector(product.previousElementSibling, '.product-travel-date');
-  const dateWithoutYear = moment.utc(day, "DD MMMM", "fr", false);
-  const dateWithoutYearString = dateWithoutYear.format('[\]DD[\/]MM[\/]([\\d*])');
-  const dateWithoutYearRegex = new RegExp(dateWithoutYearString);
-  const year = document.body.innerHTML.match(dateWithoutYearRegex);
-  const date = dateWithoutYear.set('year', year[1]).toISOString().replace(/T/, ' ');
-
-  return date;
-}
-
-function getContentByQuerySelector(element, querySelector) {
-  return element.querySelector(querySelector).innerHTML.trim();
-}
--- test.js
+++ test.js
@@ -4,15 +4,13 @@ const htmlParser = require('./html-parser.js');
 
 describe('Test parsing', () => {
   it('should return the exact same json', () => {
-      const inputFile = 'test.html';
-      const testFile = 'test-result.json';
+    const inputFile = 'test.html';
+    const testFile = 'test-result.json';
 
-      return htmlParser.loadAndParseHtml(inputFile)
-        .then(function (parsedHtml) {
-          return fs.readFileAsync(testFile, 'utf8')
-            .then(function (expectedResult) {
-              expect(JSON.parse(expectedResult)).toMatchObject(parsedHtml);
-            });
-        });
+    return htmlParser.loadAndParseHtml(inputFile)
+      .then(parsedHtml => fs.readFileAsync(testFile, 'utf8')
+        .then((expectedResult) => {
+          expect(JSON.parse(expectedResult)).toMatchObject(parsedHtml);
+        }));
   });
 });
